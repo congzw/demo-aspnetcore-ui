34e6c1e5498c5513a214d51d07e8bbe5356265b7
congzw <2981810300@qq.com>
2020-08-10 14:34:46 +0800

    add require demos

---
+++ src/NbSites.Web/wwwroot/content/demo/require-ele/app.js
@@ -0,0 +1,28 @@
+﻿requirejs.config({
+    paths: {
+        "vue": "/content/libs/vue/vue",
+        "ELEMENT": "/content/libs/element-ui/index"
+    },
+    shim: {
+        "vue": { "exports": "Vue" },
+        "ELEMENT": { "exports": "ELEMENT", "depends": "vue" }
+    }
+});
+
+require(["vue", "ELEMENT"], function (Vue, ele) {
+
+    //console.log(ele);
+
+    //注意几个坑：
+    //由于element-ui 内部写的 define("ELEMENT",["vue"],xxx)
+    //所以它的别名必须叫ELEMENT 也必须有一个叫vue的依赖才能加载
+
+    //1: vue的名称及其大小写
+    //2：ELEMENT的名称及其大小写
+    //3：Vue.use(ele);
+
+    Vue.use(ele);
+    var app = new Vue({
+        el: "#app"
+    });
+});
---
+++ src/NbSites.Web/wwwroot/content/demo/require-loader/app.js
@@ -0,0 +1,27 @@
+﻿requirejs.config({
+    paths: {
+        "vue": "/content/libs/vue/vue",
+        "ELEMENT": "/content/libs/element-ui/index",
+        "httpVueLoader": "/content/libs/vue/httpVueLoader"
+    },
+    shim: {
+        "vue": { "exports": "Vue" },
+        "ELEMENT": { "exports": "ELEMENT", "depends": "vue" },
+        "httpVueLoader": { "exports": "httpVueLoader", "depends": "vue" }
+    }
+});
+
+require(["vue", "ELEMENT", "httpVueLoader"], function (Vue, ele, httpVueLoader) {
+
+    //console.log(Vue);
+    //console.log(httpVueLoader);
+    //console.log(ele);
+    Vue.use(ele);
+    
+    var app = new Vue({
+        el: "#app",
+        components: {
+            'demo-timeline': httpVueLoader('./demo-timeline.vue')
+        }
+    });
+});
