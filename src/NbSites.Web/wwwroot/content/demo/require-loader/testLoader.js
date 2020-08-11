0683436aa3ac14eb86b23bd61bc28233761f17e6
congzw <2981810300@qq.com>
2020-08-11 16:29:21 +0800

    add testLoader

--- src/NbSites.Web/wwwroot/content/demo/require-loader/testLoader.js
+++ src/NbSites.Web/wwwroot/content/demo/require-loader/testLoader.js
@@ -15,19 +15,17 @@ require(["vue", "ELEMENT", "httpVueLoader"], function (Vue, ele, httpVueLoader) 
 
     Vue.use(ele);
 
-    ////demo for global register
-    //Vue.component('demo-timeline', httpVueLoader('./demo-timeline.vue'));
-    //var app = new Vue({
-    //    el: "#app"
-    //});
-    
-    //demo for separate register
-    var app = new Vue({
-        el: "#app",
-        components: {
-            'demo-timeline': httpVueLoader('./demo-timeline.vue')
-        }
+    //todo: load from config or init
+    var registry = [{ id: "demo-timeline", url: "./demo-timeline.vue" }];
+    registry.forEach(function (c) {
+        //register as global like ELEMENT, to support complex scenario e.g. nested components 
+        Vue.component(c.id, httpVueLoader(c.url));
     });
 
+    var app = new Vue({
+        el: "#app"
+    });
+    
+    //console.log(app.$options.components);
     return app;
 });
