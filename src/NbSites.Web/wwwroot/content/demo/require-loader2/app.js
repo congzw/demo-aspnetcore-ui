02951879acac0885c3b97f15f70609f723fc1054
congzw <2981810300@qq.com>
2020-08-11 14:29:14 +0800

    add http vue loader demo2

---
+++ src/NbSites.Web/wwwroot/content/demo/require-loader2/app.js
@@ -0,0 +1,39 @@
+﻿requirejs.config({
+    //urlArgs: "v=20200811",
+    paths: {
+        "vue": "/content/libs/vue/vue",
+        "ELEMENT": "/content/libs/element-ui/index",
+        "httpVueLoader": "/content/libs/vue/httpVueLoader"
+    },
+    shim: {
+        "vue": { "exports": "Vue" },
+        "ELEMENT": { "exports": "ELEMENT", "depends": "vue" },
+        "httpVueLoader": { "exports": "httpVueLoader", "depends": "vue" }
+    }
+});
+
+//require(["vue", "ELEMENT", "httpVueLoader"], function (Vue, ele, httpVueLoader) {
+
+//    //全局注册
+//    Vue.use(ele);
+//    var app = new Vue({
+//        el: "#app",
+//        components: {
+//            'demo-timeline': httpVueLoader('./demo-timeline.vue'),
+//            'demo-complex': httpVueLoader('./demo-complex.vue')
+//        }
+//    });
+//    return app;
+//});
+
+require(["vue", "ELEMENT", "httpVueLoader"], function (Vue, ele, httpVueLoader) {
+
+    //全局注册
+    Vue.use(ele);
+    Vue.component('demo-timeline', httpVueLoader('./demo-timeline.vue'));
+    Vue.component('demo-complex', httpVueLoader('./demo-complex.vue'));
+    var app = new Vue({
+        el: "#app"
+    });
+    return app;
+});
---
+++ src/NbSites.Web/wwwroot/content/demo/require-loader2/demo-complex.vue
@@ -0,0 +1,22 @@
+﻿<template>
+    <div class="block">
+        <h2>Complex DEMO</h2>
+        <hr />
+        <demo-timeline foo="Hello"></demo-timeline>
+    </div>
+</template>
+<style>
+</style>
+<script>
+    module.exports = {
+        //全局注册的组件，组件内部将不用再次引用
+        //components: {
+        //    'demo-timeline': httpVueLoader('./demo-timeline.vue')
+        //},
+        data() {
+            return {
+                foo: 'foo'
+            };
+        }
+    };
+</script>
